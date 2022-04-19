b7ef4b1d389060c6ae4a107d38831176fb317821
Cleverton Inacio <clev.inacio@gmail.com>
2022-04-19 19:47:47 -0300

    Atualizações: - Cria pasta views - Cria view com conteúdo da primeira aula - Cria view com todo list

--- tutorial_flutter/lib/main.dart
+++ tutorial_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tutorial_flutter/views/TodoListPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,72 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: MyHomePage());
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int count = 0;
-
-  _onClick() {
-    setState(() {
-      count++;
-    });
-  }
-
-  _reset() {
-    setState(() {
-      count = 0;
-    });
-  }
-
-  Widget getBigButton(int c) {
-    return Container(
-        height: 300,
-        width: 300,
-        decoration:
-            const BoxDecoration(color: Colors.blue, shape: BoxShape.circle),
-        child: getCounter(c));
-  }
-
-  Widget getCounter(int c) {
-    return Center(
-        child: Text(
-      "Você clicou no botão \n $count\n vezes",
-      style: const TextStyle(
-          fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
-      textAlign: TextAlign.center,
-    ));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text("Olá, Mundo!"),
-      ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          GestureDetector(
-            onTap: _onClick,
-            child: getBigButton(count),
-          )
-        ],
-      )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (() => _reset()),
-        child: const Text('limpar'),
-      ),
-    );
+    return const MaterialApp(home: TodoListPage());
   }
 }
