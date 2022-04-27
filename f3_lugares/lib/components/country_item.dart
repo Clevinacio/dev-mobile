1d94c1940777675e9ddb90f3839ad1b72494c4b7
Cleverton Inacio <clev.inacio@gmail.com>
2022-04-26 21:06:34 -0300

    Adiciona novo tutorial

---
+++ f3_lugares/lib/components/country_item.dart
@@ -0,0 +1,43 @@
+import 'package:f3_lugares/screens/countries_places_screen.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import '../models/country.dart';
+
+class CountryItem extends StatelessWidget {
+  final Country country;
+
+  const CountryItem(this.country);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(15),
+      height: double.infinity,
+      width: double.infinity,
+      child: Text(
+        country.title,
+        style: Theme.of(context).textTheme.headline6,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        gradient: LinearGradient(
+            colors: [
+              country.color.withOpacity(0.5), //cor passad com opacidade
+              country.color, //cor passada
+            ],
+            begin: Alignment.topLeft, //inicio do gradiente
+            end: Alignment.bottomRight // fim
+            ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ f3_lugares/lib/main.dart
@@ -0,0 +1,32 @@
+import 'dart:ui';
+
+import 'package:f3_lugares/screens/countries_places_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/countries_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PlacesToGo',
+      theme: ThemeData(
+          colorScheme: ThemeData()
+              .colorScheme
+              .copyWith(primary: Colors.purple, secondary: Colors.amber),
+          //primarySwatch: Colors.purple,
+          //accentColor: Colors.amber,
+          fontFamily: 'Roboto',
+          canvasColor: Color.fromARGB(255, 255, 255, 255),
+          textTheme: ThemeData.light().textTheme.copyWith(
+                  headline6: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ))),
+      debugShowCheckedModeBanner: false,
+      home: CountriesScreen(),
+    );
+  }
+}
